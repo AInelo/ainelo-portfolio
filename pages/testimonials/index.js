b45e8d7a752cdb6b49c6bc69c2c5e553c4bb2be8
Lionel TOTON <totonlionel@gmail.com>
2024-06-30 01:00:58 +0200

    lionel

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -7,6 +7,8 @@ import TestimonialSlider from "../../components/TestimonialSlider";
 import {motion} from 'framer-motion'
 import { fadeIn } from '../../variants'
 const Testimonials = () => {
+
+  
   return( 
   <div className="h-full bg-primary/30 py-32 text-center" >
       <div className="container mx-auto h-full flex flex-col justify-center">
